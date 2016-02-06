3881cddc2d7d544b8b1626aa0cb81fae54d25fcb
alejandi <alejotesa@hotmail.com>
2016-02-06 13:34:40 -0500

    Programa para identificar si un carro tiene pico y placa

---
+++ PicoYplaca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYplaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para identificar si tiene Pico y placa o NO.");
+            Console.WriteLine("Ingrese su placa");
+            var placa = int.Parse(Console.ReadLine());
+            
+
+        }
+    }
+}
