cdb3f7ff0f510db2df3c89a2e63047faba562c87
alejandi <alejotesa@hotmail.com>
2016-02-08 21:38:26 -0500

    se agrego el if con el cual se identifica que automóvil puede salir sin restricción de pico y placa.

--- PicoYplaca/Program.cs
+++ PicoYplaca/Program.cs
@@ -12,7 +12,26 @@ namespace PicoYplaca
         {
             Console.WriteLine("Programa para identificar si tiene Pico y placa o NO.");
             Console.WriteLine("Ingrese su placa");
-            var placa = int.Parse(Console.ReadLine());
+            var placa = Console.ReadLine();
+            var placaNumero = int.Parse(placa.Substring(5));
+            int modPlacaNumero = placaNumero % 2;
+            int dia = DateTime.Now.Day;
+            int modDia = dia % 2;
+
+            if ( modPlacaNumero == 0 && modDia == 0)
+
+            {
+                Console.WriteLine($"No puede salir el automóvil de placa {placa} ");
+
+            }    
+            else
+            {
+                Console.WriteLine($"Puede salir el automóvil de placa {placa} ");
+
+            }        
+            
+            Console.ReadLine();
+
             
 
         }
