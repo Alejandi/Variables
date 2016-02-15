40c52bdad5ba492c02d542f3c0e248c141581512
alejandi <alejotesa@hotmail.com>
2016-02-14 21:10:07 -0500

    Se agrega programa de impresion de numeros impares usando un arreglo, un if y dos for.

---
+++ ImpresionImpares/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpresionImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[] { 1, 2, 3, 4, 8, 7 };
+            for (int i = 0; i <= 5; i++)
+            {
+                if (numeros[i] % 2 == 1)
+                {
+                    for (int j = 0; j < i + 1; j++)
+
+                    {
+                        Console.Write(numeros[i]);
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MaximoDosNumeros/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximoDosNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer valor");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo valor");
+            var b = double.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine("Los números son iguales");
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                double numeromayor = maxDosNumeros(a, b);
+                Console.WriteLine($"El número mayor es {numeromayor}");
+                Console.ReadLine();
+            }
+        }
+        private static double maxDosNumeros(double a, double b)
+        {
+            //if (a > b)
+            //{
+            //    return a;
+            //}
+
+            //else
+            //{
+            //    return b;
+            //}
+
+            return a > b ? a : b;
+        }
+
+
+
+    }
+}
